5e0933e09f81acf4e008544dc0d999376c252c88
fabio <parisifabio98@gmail.com>
2021-03-11 15:10:43 +0100

    hamburger legera ottimizzazione del codice

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 console.log("ciao ale");
 
 //inizializzazione variabile che si riferisce all'div da aprire e chiudere
-var menu = ".hamburger-menu"
+var menu = $(".hamburger-menu");
 
 //puntamento al bottone/icona del hamburger-menu
 $(".fas.fa-bars").click(function () {
 	//puntamento all'hamburgher-menu ed successiva apertura
-	$(menu).show()
+	menu.show()
 })
 
 //puntamento al bottone/icona del chiusura dell'hamburger-menu
 $(".fas.fa-times").click(function () {
 	//puntamento all'hamburgher-menu ed successiva chiusura
-	$(menu).hide()
+	menu.hide()
 })
