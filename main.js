07c1c0098087a360f8ead55379c172d160053e4b
fabio <parisifabio98@gmail.com>
2021-03-11 09:41:26 +0100

    hamburger aggiunta commenti

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 console.log("ciao ale");
+
+//inizializzazione variabile che si riferisce all'div da aprire e chiudere
+var menu = ".hamburger-menu"
+
+//puntamento al bottone/icona del hamburger-menu
 $(".fas.fa-bars").click(function () {
-	$(".hamburger-menu").show()
+	//puntamento all'hamburgher-menu ed successiva apertura
+	$(menu).show()
 })
 
+//puntamento al bottone/icona del chiusura dell'hamburger-menu
 $(".fas.fa-times").click(function () {
-	$(".hamburger-menu").hide()
+	//puntamento all'hamburgher-menu ed successiva chiusura
+	$(menu).hide()
 })
